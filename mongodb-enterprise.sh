2f816f65b579a3b59986bb415b822b5521305332
shortthirdman (Swetank Mohanty) <swetank.mohanty@outlook.com>
2020-04-28 17:14:04 +0530

    Added commands for PowerShell
    Signed-off-by: shortthirdman (Swetank Mohanty) <swetank.mohanty@outlook.com>

--- mongodb-enterprise.sh
+++ mongodb-enterprise.sh
@@ -1,11 +1,14 @@
+## Set-Variable -Name "MONGODB_VERSION" -Value "4.2"
 export MONGODB_VERSION=4.2
+
 curl -O --remote-name-all https://raw.githubusercontent.com/docker-library/mongo/master/$MONGODB_VERSION/{Dockerfile,docker-entrypoint.sh}
 
+## Set-Variable -Name "DOCKER_USERNAME" -Value "shortthirdman"
 export DOCKER_USERNAME=shortthirdman
 chmod 755 ./docker-entrypoint.sh
 docker build --build-arg MONGO_PACKAGE=mongodb-enterprise --build-arg MONGO_REPO=repo.mongodb.com -t $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION .
 
-docker run --name mymongo -itd $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION
-docker exec -it mymongo /usr/bin/mongo --eval "db.version()"
+docker run --name mongodb-ee -itd $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION
+docker exec -it mongodb-ee /usr/bin/mongo --eval "db.version()"
 
 docker push $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION
